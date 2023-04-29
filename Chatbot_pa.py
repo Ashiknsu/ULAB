103be102e52401f209fec605fc5c3223a03415bd
Ashiknsu <34792272+Ashiknsu@users.noreply.github.com>
2023-04-29 16:36:42 +0600

    Chatbot_pa.py

--- Chatbot_pa.py
+++ Chatbot_pa.py
@@ -4,9 +4,14 @@ import streamlit as st
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 st.header("CHATBOT")
-prompt = st.text_input("Prompt", "Enter your message here...")
+prompt = st.text_input("Prompt")
 
-if st.button("Send"):
+
+st.sidebar.title("Chatbot")
+st.sidebar.button("ULAB")
+st.sidebar.button("ChatGPT")
+
+if st.sidebar.button("ChatGPT") and st.button("Send"):
     with st.spinner("Generating response..."):
         st.session_state["messages"] += [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
@@ -17,7 +22,9 @@ if st.button("Send"):
             {"role": "system", "content": message_response}
         ]
         show_messages(text)
-st.sidebar.title("Chatbot")
+
+
+
 def show_messages(text):
     messages_str = [
         f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
